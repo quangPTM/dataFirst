42d2dade25c333a2fd87efd6b8ea832c90b4d6df
quangPTM <quangmtp@gmail.com>
2023-10-24 21:58:52 +0700

    datafirst

---
+++ DemoDatabaseFirst/DemoDatabaseFirst/Models/MyStoreContext.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoDatabaseFirst.Models;
+
+public partial class MyStoreContext : DbContext
+{
+    public MyStoreContext()
+    {
+    }
+
+    public MyStoreContext(DbContextOptions<MyStoreContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=QUANG; Database=MyStore; User id=sa; Password=123; TrustServerCertificate=true; Trusted_Connection=SSPI; Encrypt=false;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__Categori__19093A2BC7858BEC");
+
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.CategoryName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__Products__B40CC6ED720EBA75");
+
+            entity.Property(e => e.ProductId).HasColumnName("ProductID");
+            entity.Property(e => e.CategoryId).HasColumnName("CategoryID");
+            entity.Property(e => e.ProductName)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.UnitPrice).HasColumnType("decimal(10, 2)");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products)
+                .HasForeignKey(d => d.CategoryId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Products__Catego__398D8EEE");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ DemoDatabaseFirst/DemoDatabaseFirst/Program.cs
@@ -0,0 +1,26 @@
+﻿using DemoDatabaseFirst.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemeDatabaseFirst {
+class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStoreContext myStore =new MyStoreContext();
+            var products= from p in myStore.Products
+                          select new {p.ProductName, p.CategoryId};
+            foreach (var p in products) { 
+            Console.WriteLine($"ProductName: {p.ProductName}, CategoruID: {p.CategoryId}");
+            }
+            Console.WriteLine("--------------------");
+            IQueryable<Category> cats=myStore.Categories.Include(c=>c.Products);
+            foreach (Category c in cats) {
+                Console.WriteLine($"CategoryID: {c.CategoryId} has {c.Products.Count} products.");
+            }
+            Console.ReadLine();
+        }
+    
+    }
+
+}
+
